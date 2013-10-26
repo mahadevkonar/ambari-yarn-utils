f1d2fe20b3b5fedbe760ab66f9d7bd5236636a52
Mahadev Konar <mahadev@apache.org>
2013-10-26 11:11:37 -0700

    YARN Utils

---
+++ yarn-utils/yarn-utils.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+'''
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import optparse
+from pprint import pprint
+import logging
+import sys
+import math
+
+''' Reserved for OS + DN + NM,  Map: Memory => Reservation '''
+reservedStack = { 4:1, 8:2, 16:2, 24:4, 48:6, 64:8, 72:8, 96:12, 
+                   128:24, 256:32, 512:64}
+''' Reserved for HBase. Map: Memory => Reservation '''
+  
+reservedHBase = {4:1, 8:1, 16:2, 24:4, 48:8, 64:8, 72:8, 96:16, 
+                   128:24, 256:32, 512:64}
+GB = 1024
+
+def getMinContainerSize(memory):
+  if (memory <= 4):
+    return 256
+  elif (memory <= 8):
+    return 512
+  elif (memory <= 24):
+    return 1024
+  else:
+    return 2048
+  pass
+
+def getReservedStackMemory(memory):
+  if (reservedHBase.has_key(memory)):
+    return reservedHBase[memory]
+  if (memory <= 4):
+    ret = 1
+  elif (memory >= 512):
+    ret = 64
+  else:
+    ret = 0
+  return ret
+
+def getReservedHBaseMem(memory):
+  if (reservedHBase.has_key(memory)):
+    return reservedHBase[memory]
+  if (memory <= 4):
+    ret = 1
+  elif (memory >= 512):
+    ret = 64
+  else:
+    ret = 0
+  return ret
+                    
+def main():
+  log = logging.getLogger(__name__)
+  out_hdlr = logging.StreamHandler(sys.stdout)
+  out_hdlr.setFormatter(logging.Formatter(' %(message)s'))
+  out_hdlr.setLevel(logging.INFO)
+  log.addHandler(out_hdlr)
+  log.setLevel(logging.INFO)
+  parser = optparse.OptionParser()
+  parser.add_option('-c', '--cores', default = 4,
+                     help = 'Number of cores on each host')
+  parser.add_option('-m', '--memory', default = 64, 
+                    help = 'Amount of Memory on each host in GB')
+  parser.add_option('-d', '--disks', default = 4, 
+                    help = 'Number of disks on each host')
+  parser.add_option('-k', '--hbase', default = True,
+                    help = 'True if HBase is installed, False is not')
+  (options, args) = parser.parse_args()
+  
+  cores = options.cores
+  memory = options.memory
+  disks = options.disks
+  hbaseEnabled = options.hbase
+  log.info("Using cores=" +  str(cores) + " memory=" + str(memory) + "(GB)" +
+            " disks=" + str(disks) + " hbase=" + str(hbaseEnabled))
+  minContainerSize = getMinContainerSize(memory)
+  reservedStackMemory = getReservedStackMemory(memory)
+  reservedHBaseMemory = 0
+  if (hbaseEnabled):
+    reservedHBaseMemory = getReservedHBaseMem(memory)
+  reservedMem = reservedStackMemory + reservedHBaseMemory
+  log.info("Profile: cores=" + str(cores) + " memory=" + str(memory) 
+           + " reserved=" + str(reservedMem) + "GB" + " usableMem="
+           + str(memory - reservedMem) + "GB" + " disks=" + str(disks))
+  memory -= (reservedStackMemory + reservedHBaseMemory)
+  memory *= GB
+  containers = max(2* cores, min(abs(math.ceil(1.8 * disks)), memory/minContainerSize))
+  container_ram =  abs(memory/containers)
+  if (container_ram > GB):
+    container_ram = container_ram / 512 * 512
+  log.info("Num Container=" + str(containers))
+  log.info("Container Ram=" + str(container_ram))
+  log.info("User Ram=" + str(containers*container_ram/float(GB)))
+  log.info("Unused Ram=" + str(float(memory) - (containers*container_ram)/GB))
+  log.info("yarn.scheduler.minimum-allocation-mb=" + str(container_ram))
+  log.info("yarn.scheduler.maximum-allocation-mb=" + str(containers*container_ram))
+  log.info("yarn.nodemanager.resource.memory-mb=" + str(containers*container_ram))
+  map_memory = container_ram
+  reduce_memory = 2*container_ram if (container_ram <= 2048) else container_ram
+  am_memory = max(map_memory, reduce_memory)
+  log.info("mapreduce.map.memory.mb=" + str(map_memory))
+  log.info("mapreduce.map.java.opts=" + str(abs(0.8 * map_memory)))
+  log.info("mapreduce.reduce.memory.mb=" + str(reduce_memory))
+  log.info("mapreduce.reduce.java.opts=" + str(abs(0.8 * reduce_memory)))
+  log.info("yarn.app.mapreduce.am.resource.mb=" + str(am_memory))
+  log.info("yarn.app.mapreduce.am.command-opts=" + str(abs(0.8*am_memory)))
+  log.info("io.sort.mb=" + str(abs(0.4 * map_memory)))
+  pass
+
+if __name__ == '__main__':
+  try:
+    main()
+  except(KeyboardInterrupt, EOFError):
+    print("\nAborting ... Keyboard Interrupt.")
+    sys.exit(1)
